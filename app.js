1b92a38a0f9709928e82e4bd33ce290ec6958781
Sarah Smith <sarahlib90@gmail.com>
2025-11-18 19:03:44 -0500

    Added html, css & pseudocode in js

---
+++ app.js
@@ -0,0 +1,33 @@
+// Define constants and variables.
+// Define the app’s state variables, but don’t assign values to them.
+    // Define a variable for the user's choice
+    // Define a variable for the computer's choice
+    // Define a variable for the game message
+// Select and save (cache) elements in variables that need to be accessed in the JavaScript code more than once.
+    // select results display element
+// Add event listeners - use delegated event listeners to listen to multiple elements with a single listener.
+    // Using the event listeners set up, assign the user's choice to the variable for the user's choice
+    // Invoke get player choice function from play function
+// Invoke the init function used to initialize all state variables.
+// Invoke the primary render function that transfers all state variables to the DOM.
+    // Render the game message to the DOM
+// Wait for the user to make letter selection
+// Update all state variables with the correct values depending on the user’s choice.
+    // add letter to DOM if part of word
+    // reduce guesses to DOM if not part of word
+    // eliminate that letter from guesses to avoid duplicate guessing letter
+// Invoke the primary render function.
+// Wait for the user to click the “Play Again” button.
+    // reset users guesses to nothing
+    // reset game message & status
+// Invoke the init function to reset all state variables to their initial values.
+
+/* ----- CONSTANTS ----- */
+
+/* ----- VARIABLES/STATE ----- */
+
+/* ----- CACHED ELEMENTS ----- */
+
+/* ----- FUNCTIONS ----- */
+
+/* ----- EVENT LISTENERS ----- */
